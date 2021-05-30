3c9d300fe2957dc485402c8a6a8bbc799f213122
sparsh.singh23@gmail.com <sparsh.singh23@gmail.com>
2021-05-30 09:04:25 +0530

    initial commit

---
+++ otpFramework/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  otpFramework
+//
+//  Created by Sparsh Singh on 30/05/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    
+
+}
---
+++ otpFramework/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  otpFramework
+//
+//  Created by Sparsh Singh on 30/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.systemYellow
+    }
+
+
+}
+
